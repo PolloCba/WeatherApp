8e370547098c5ba1b390c45604a6314ee75fa346
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-06-17 11:04:55 -0300

    Proyecto Weather

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
   }
   
   function onSearch(ciudad) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
