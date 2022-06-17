5dc3331b4417807db1b0a1526bb36496e8352dcb
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-06-17 15:41:33 -0300

    Proyecto Weather App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import './App.css';
-import Cards from './components/Cards.jsx';
-import Nav from './components/Nav.jsx';
-import { Route } from 'react-router-dom';
-import About from './components/About.jsx';
-import City from './components/City.jsx';
+import React, { useState } from "react";
+import "./App.css";
+import Cards from "./components/Cards.jsx";
+import Nav from "./components/Nav.jsx";
+import { Route } from "react-router-dom";
+import About from "./components/About.jsx";
+import City from "./components/City.jsx";
 
-const API_KEY = "7255db7793802f2a696a5c4e2a1e9b45"
+const API_KEY = "7255db7793802f2a696a5c4e2a1e9b45";
 
 function App() {
   const [cities, setCities] = useState([]);
   function onClose(id) {
-    setCities(oldCities => oldCities.filter(c => c.id !== id));
+    setCities((oldCities) => oldCities.filter((c) => c.id !== id));
   }
+
+  function addCity(city) {
+    setCities((prevCities) => {
+      return [city, ...prevCities];
+    });
+  }
+
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`)
-      .then(r => r.json())
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${API_KEY}&units=metric`
+    )
+      .then((r) => r.json())
       .then((recurso) => {
-        if(recurso.main !== undefined){
+        if (recurso.main !== undefined) {
           const ciudad = {
             min: Math.round(recurso.main.temp_min),
             max: Math.round(recurso.main.temp_max),
@@ -30,20 +39,23 @@ function App() {
             weather: recurso.weather[0].main,
             clouds: recurso.clouds.all,
             latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
+            longitud: recurso.coord.lon,
           };
-          setCities(oldCities => [...oldCities, ciudad]);
+          const oldCity = cities.find((city) => city.id === ciudad.id);
+          if (!oldCity) {
+            addCity(ciudad);
+          } else {
+            alert("La ciudad ya fue proporcionada");
+          }
         } else {
           alert("Ciudad no encontrada");
         }
       });
   }
   function onFilter(ciudadId) {
-    const cityToSearch = cities.find(
-      function(city){
-        return city.id === parseInt(ciudadId)
-      }
-      )
+    const cityToSearch = cities.find(function (city) {
+      return city.id === parseInt(ciudadId);
+    });
 
     // if(cityToSearch){
     //   return cityToSearch
@@ -51,37 +63,28 @@ function App() {
     //   return null
     // }
     // return cityToSearch  ? cityToSearch : undefined
-    return cityToSearch
+    return cityToSearch;
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <Route exact  path="/">
-          <div>
-            <Cards
-              cities={cities}
-              onClose={onClose}
-            />
-          </div>
+      <Nav onSearch={onSearch} />
+      <Route exact path="/">
+        <div>
+          <Cards cities={cities} onClose={onClose} />
+        </div>
       </Route>
-      <Route path="/about"  component={About} />
-      <Route  
-          path="/city/:cityId" 
-          render={
-            function (props){
-              return <City
-                city={
-                    onFilter(props.match.params.cityId)
-                }
+      <Route path="/about" component={About} />
+      <Route
+        path="/city/:cityId"
+        render={function (props) {
+          return (
+            <City
+              city={onFilter(props.match.params.cityId)}
               //  params={props.match.params.cityId}
-                />
-            }
-          }
-      >
-
-      </Route>
-
-
+            />
+          );
+        }}
+      ></Route>
     </div>
   );
 }
